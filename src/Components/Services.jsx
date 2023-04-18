2611f18bd6bfdd46be1344c4240b8ca189c41a2e
UlyssesCortes <ulyssescortes13@gmail.com>
2023-04-18 01:12:35 -0700

    Service section complete

---
+++ src/Components/Services.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import '../CSS/Services.css'
+
+
+export default function Services() {
+    return (
+        <section className='services'>
+            <div className='subHeader'>
+                <p className='sectionTitle'>Services</p>
+                <div className='serviceQuote'>
+                    <div className='quotes'></div>
+                    <p>
+                        WindowPros provides with quick
+                        <br></br>
+                        and excellent results.
+                    </p>
+                    <div className='avatarImg'></div>
+                </div>
+            </div>
+
+            <div className='serviceBoxes'>
+                <div className='residential serviceBox'>
+                    <div className='homeIcon servIcon'></div>
+                    <p className='sectionSlogan'><span>Residential</span>
+                        <br></br>
+                        Home window
+                        <br></br>
+                        cleaning
+                    </p>
+                    <section className='bottomCard'>
+                        <div className='plusIcon2 '></div>
+                        <button className='learnMoreBtn residentialLearn'>Learn More</button>
+                    </section>
+
+                </div>
+                <div className='serviceBox'>
+                    <div className='buildingIcon servIcon'></div>
+                    <p className='sectionSlogan'><span>Commercial</span>
+                        <br></br>
+                        Buildings window
+                        <br></br>
+                        cleaning
+                    </p>
+                    <section className='bottomCard'>
+                        <div className='plusIcon'></div>
+                        <button className='learnMoreBtn'>Learn More</button>
+                    </section>
+
+                </div>
+                <div className='serviceBox'>
+                    <div className='solarIcon servIcon'></div>
+                    <p className='sectionSlogan'><span>Solar</span>
+                        <br></br>
+                        Panel cleaning and
+                        <br></br>
+                        energi saving
+                    </p>
+                    <section className='bottomCard'>
+                        <div className='plusIcon'></div>
+                        <button className='learnMoreBtn'>Learn More</button>
+                    </section>
+                </div>
+            </div>
+        </section>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import Navbar from './Components/Navbar'
 import Hero from './Components/Hero'
+import Services from './Components/Services'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Navbar />
     <Hero />
+    <Services />
   </React.StrictMode>,
 )
